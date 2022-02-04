e51d91840114bf078ecd6b429763810035e4ff9a
Chris Coy <christophercoydev@gmail.com>
2022-02-03 21:51:52 -0600

    Adding skeleton projects for Spring

---
+++ load-balancer/src/test/java/com/sailfishlabs/examples/cloud/loadbalancer/LoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sailfishlabs.examples.cloud.loadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
